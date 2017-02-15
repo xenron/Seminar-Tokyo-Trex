8e610dba1ed60106a4a777a153fb3becc531d637
xenron <xenron@hotmail.com>
2017-02-15 21:45:32 +0900

    msg

--- docker/base/build-image-root.sh
+++ docker/base/build-image-root.sh
@@ -36,48 +36,54 @@ docker images > images.txt
 #all image is based on dnsmasq. master and slaves are based on base image.
 if [ $image == "base-dnsmasq" ]
 then
-	docker_rmi hadoop-master
-	docker_rmi hadoop-slave
-	docker_rmi hadoop-base
-	docker_rmi base-dnsmasq
-	docker_build base-dnsmasq
-	docker_build hadoop-base
-	docker_build hadoop-master
-	docker_build hadoop-slave 
+    docker_rmi hbase-master
+    docker_rmi hbase-slave
+    docker_rmi hbase-base
+    docker_rmi hadoop-master
+    docker_rmi hadoop-slave
+    docker_rmi hadoop-base
+    docker_rmi base-dnsmasq
+    docker_build base-dnsmasq
+    docker_build hadoop-base
+    docker_build hadoop-master
+    docker_build hadoop-slave 
+    docker_build hbase-base
+    docker_build hbase-master
+    docker_build hbase-slave 
 elif [ $image == "hadoop-base" ]
 then
-	docker_rmi hadoop-master
-	docker_rmi hadoop-slave
-	docker_rmi hadoop-base
-	docker_build hadoop-base
-	docker_build hadoop-master
-	docker_build hadoop-slave
+    docker_rmi hadoop-master
+    docker_rmi hadoop-slave
+    docker_rmi hadoop-base
+    docker_build hadoop-base
+    docker_build hadoop-master
+    docker_build hadoop-slave
 elif [ $image == "hadoop-master" ]
 then
-	docker_rmi hadoop-master
-	docker_build hadoop-master
+    docker_rmi hadoop-master
+    docker_build hadoop-master
 elif [ $image == "hadoop-slave" ]
 then
-	docker_rmi hadoop-slave
-	docker_build hadoop-slave
+    docker_rmi hadoop-slave
+    docker_build hadoop-slave
 elif [ $image == "hbase-base" ]
 then
-	docker_rmi hbase-master
-	docker_rmi hbase-slave
-	docker_rmi hbase-base
-	docker_build hbase-base
-	docker_build hbase-master
-	docker_build hbase-slave
+    docker_rmi hbase-master
+    docker_rmi hbase-slave
+    docker_rmi hbase-base
+    docker_build hbase-base
+    docker_build hbase-master
+    docker_build hbase-slave
 elif [ $image == "hbase-master" ]
 then
-	docker_rmi hbase-master
-	docker_build hbase-master
+    docker_rmi hbase-master
+    docker_build hbase-master
 elif [ $image == "hbase-slave" ]
 then
-	docker_rmi hbase-slave
-	docker_build hbase-slave
+    docker_rmi hbase-slave
+    docker_build hbase-slave
 else
-	echo "The image name is wrong!"
+    echo "The image name is wrong!"
 fi
 
 #docker_rmi hadoop-base
